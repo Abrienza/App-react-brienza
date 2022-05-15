b8b9cae82625da5771b7ae68bcf0c1f077595f13
ayelen <ayelen.brienza@gmail.com>
2022-05-15 10:34:32 -0300

    redondeando cards

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,12 @@
 import React from "react"
-import {Card, Button, Container, Row, Col} from "react-bootstrap"
+import {Card, Button, Container} from "react-bootstrap"
 import ItemCount from "../ItemCount/ItemCount"
 
 export default function Item ({product}) {
     return (
-        <Container>
-                    <Card className="item" style={{ width: '18rem', border: "2px solid #000" }}>
-                    <Card.Img variant="top" src={product.image}/>
+        <div>
+                    <Card className="item" style={{ width: '18rem', border: "2px solid #000", margin: "1rem" }}>
+                    <Card.Img variant="top" src={product.image} alt={product.name}/>
                     <Card.Body>
                         <Card.Title>{product.title}</Card.Title>
                         <Card.Text>
@@ -25,6 +25,6 @@ export default function Item ({product}) {
 
                     </Card.Body>
                     </Card>
-        </Container>
+        </div>
     )
 }
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,13 +3,19 @@ import Item from "../Item/Item"
 
 
 export default function ItemList ({products}) {
+    
     return (
-        <div>
+        <>
+        <h3>
+            Lista de Productos
+        </h3>
+        <div style={{display:'flex', justifyContent:'space-between', flexWrap:'wrap',alignItems:'center'}}>
             {products.map((product, index) => {
                 return (
                     <Item product={product} key={product.id}/>
                 )
                 })}
         </div>
+        </>
     )
 }
